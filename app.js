0e6a982db9d43d1cc93ef3a6ac7ba16214f840a8
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2022-07-26 08:14:25 -0600

    changed html to .ejs & expressLayouts

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT;
+app.use(cors({ origin: `http://localhost:${PORT}` }));
+
+// static directories
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+
+// Templating Engine
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Routing
+app.get("", (request, response) => {
+  response.render("index");
+});
+
+// Login route
+app.get("/login", (request, response) => {
+  response.render("login");
+});
+
+app.listen(PORT || 3000, () => console.log(`Listening on port: ${PORT}`));
